4736510e0380239fa104a3d34a5671688e5f99fa
Prajesh Gaikwad <102733514+prajesh8484@users.noreply.github.com>
2024-03-03 19:56:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function addTask() {
+    var input = document.getElementById("taskInput");
+    var task = input.value.trim();
+    
+    if (task === "") {
+        alert("Please enter a task!");
+        return;
+    }
+    
+    var taskList = document.getElementById("taskList");
+    var li = document.createElement("li");
+    li.innerHTML = task + '<span class="delete" onclick="deleteTask(this)"> &times;</span>';
+    taskList.appendChild(li);
+    
+    input.value = "";
+}
+
+function deleteTask(taskElement) {
+    var li = taskElement.parentElement;
+    li.remove();
+}
 
