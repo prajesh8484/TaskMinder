6454806cdeb3816b5da4dee3111a87921ebbb5b5
Prajesh Gaikwad <102733514+prajesh8484@users.noreply.github.com>
2024-06-11 23:21:02 +0530

    ✨ feat: Add local storage for persisting tasks

--- script.js
+++ script.js
@@ -13,11 +13,22 @@ function addTask() {
     taskList.appendChild(li);
     
     input.value = "";
+    updateTaskListInLocalStorage(); 
 }
 
 function deleteTask(taskElement) {
     var li = taskElement.parentElement;
     li.remove();
+    updateTaskListInLocalStorage(); 
+}
+
+function updateTaskListInLocalStorage() {
+    var taskList = document.getElementById("taskList");
+    var tasks = [];
+    for (var i = 0; i < taskList.children.length; i++) {
+        tasks.push(taskList.children[i].innerText);
+    }
+    localStorage.setItem("taskList", JSON.stringify(tasks));
 }
 
 document.getElementById("taskInput").addEventListener("keydown", function(event) {
@@ -31,6 +42,20 @@ document.addEventListener("keydown", function(event) {
         var taskList = document.getElementById("taskList");
         if (taskList.children.length > 0) {
             taskList.removeChild(taskList.children[0]);
+            updateTaskListInLocalStorage(); 
         }
     }
 });
+
+window.onload = function() {
+    var storedTasks = localStorage.getItem("taskList");
+    if (storedTasks) {
+        var tasks = JSON.parse(storedTasks);
+        var taskList = document.getElementById("taskList");
+        for (var i = 0; i < tasks.length; i++) {
+            var li = document.createElement("li");
+            li.innerHTML = tasks[i] + '<span class="delete" onclick="deleteTask(this)"> &times;</span>';
+            taskList.appendChild(li);
+        }
+    }
+};
