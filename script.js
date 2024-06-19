36ac7f21a62db0d7a8f0a4b851f8e94c213df0fa
Prajesh Gaikwad <102733514+prajesh8484@users.noreply.github.com>
2024-06-19 23:08:59 +0530

    ✨ feat: Change appearance of buttons in mobile view

--- script.js
+++ script.js
@@ -59,3 +59,27 @@ window.onload = function() {
         }
     }
 };
+
+function modifyButtons() {
+    
+    var githubButton = document.querySelector('.github-button');
+    var downloadButton = document.querySelector('.download-button');
+
+    var buttonsContainer = document.querySelector('.buttons');
+    buttonsContainer.style.width = '175px';
+    buttonsContainer.style.display = 'flex';
+    buttonsContainer.style.alignItems = 'center';
+    buttonsContainer.style.justifyContent = 'space-around';
+
+    var githubImageSrc = 'assets/logo.png';
+    var downloadImageSrc = 'assets/download-2-512.png';
+
+    var imageHeight = '36px'; 
+        githubButton.innerHTML = '<img src="assets/logo.png" alt="GitHub" style="height:' + imageHeight + '; width: 35px;' + '; padding-left: 7px;' + '; padding-right: 7px;' + ';">';
+        downloadButton.innerHTML = '<img src="assets/download-2-512.png" alt="Download" style="height: 42px' + '; width: 35px;' + '; padding-left: 5px;' + '; padding-right: 5px;' + ';">';
+}
+
+var screenWidth = window.innerWidth;
+if (screenWidth < 600) {
+modifyButtons();
+}
